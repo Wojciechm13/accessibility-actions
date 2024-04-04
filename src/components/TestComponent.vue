4a70f4b750411ae2f3a5aba505336934f18d92b1
Wojciech Mielczarek <wojciechmielczarek@MacBook-Pro-Wojciech.local>
2024-04-04 13:01:04 +0200

    Add test component

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    <h1 class="test-title">TEST COMPONENT</h1>
+</template>
+
+<style scoped>
+.test-title {
+    color: red;
+}
+</style>
